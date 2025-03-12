7d67e00273ad875947aa5bfc17cc9582196f3454
bgmin2e <bgmin2e@gmail.com>
2025-03-12 23:44:07 +0900

    add: option popover box 구현

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   plugins: [
     new MiniCssExtractPlugin({
